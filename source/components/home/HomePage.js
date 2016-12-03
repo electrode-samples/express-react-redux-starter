fedba769309441a5f5e542717c83c61241ee43cc
raydecastro <captray108@gmail.com>
2016-12-02 17:45:58 -0800

    feature. added redux functionality.

--- source/components/home/HomePage.js
+++ source/components/home/HomePage.js
@@ -5,7 +5,7 @@ class HomePage extends Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1>Administration</h1>
+        <h1>Course Admin</h1>
         <p>React, Redux, and React Router in ES6 for ultra-responsive web apps.</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
